d327998e0ddc23bbd270383c4687f780a224e5af
janaka <129352300+janaka4345@users.noreply.github.com>
2023-12-17 07:08:55 +0000

    feat: Add image sprite functionality
    Add image sprite functionality and update canvas background color

--- src/components/Canvas1.jsx
+++ src/components/Canvas1.jsx
@@ -1,5 +1,8 @@
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 import { useEffect, useRef } from "react";
+let img;
+let spriteWidth;
+let spriteHeight;
 
 export default function Canvas1() {
   // useEffect(() => {
@@ -13,20 +16,36 @@ export default function Canvas1() {
 function setup(p5) {
   return () => {
     p5.createCanvas(600, 400);
+    spriteWidth = img.width / 8;
+    spriteHeight = img.height / 8;
   };
 }
 
 function draw(p5) {
   return () => {
-    p5.background(255, 0, 0);
-    p5.rect(100, 100, 100, 100);
+    console.log(spriteWidth, spriteHeight);
+    p5.background(0);
+    // p5.rect(100, 100, 100, 100);
+    p5.image(
+      img,
+      p5.width / 2,
+      p5.height / 2,
+      spriteWidth,
+      spriteHeight,
+      spriteWidth * 2,
+      spriteHeight * 0,
+      spriteWidth,
+      spriteHeight,
+    );
+    // p5.imageMode(CENTER);
+    p5.noLoop();
   };
 }
 
 function preload(p5) {
   // Preload the image
   return () => {
-    // img = p5.loadImage("./shadow_dog.png");
+    img = p5.loadImage("./checker.jpg");
   };
 }
 
