6f0ce7e0c7e653ead00b6a1c1a0ab25fab64bdd8
banuka <banukaramanayaka76@gmail.com>
2023-12-20 09:43:27 +0530

    fix: box resizing isssue and rotation

--- src/components/Canvas3.jsx
+++ src/components/Canvas3.jsx
@@ -130,13 +130,17 @@ function draw(p5) {
         // p5.fill(255, 0, 0);
         p5.push();
         p5.fill(255, 0, 0);
-        p5.rectMode(p5.CENTER);
+        // p5.rectMode(p5.CENTER);
         // p5.rotate(body.angle);
-        p5.rect(
-          body.position.x,
-          body.position.y,
-          body.bounds.max.x - body.bounds.min.x,
-          body.bounds.max.y - body.bounds.min.y
+        p5.quad(
+          body.vertices[0].x,
+          body.vertices[0].y,
+          body.vertices[1].x,
+          body.vertices[1].y,
+          body.vertices[2].x,
+          body.vertices[2].y,
+          body.vertices[3].x,
+          body.vertices[3].y
         );
         p5.pop();
       }
